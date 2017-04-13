df08b11951b57e2b5a981e0d2956fc468d4393e5
jstarling1 <jennstarling@gmail.com>
2017-04-13 14:12:57 -0500

    MCMC Ex 11

--- Sample Code_MCMC/MCMC_Ex11_Gibbs Sampler_Two Component Gaussian Mixture.R
+++ Sample Code_MCMC/MCMC_Ex11_Gibbs Sampler_Two Component Gaussian Mixture.R
@@ -1,6 +1,8 @@
 #MCMC Exercise 11 - Problem 1 & 2
 #Mixture Model with Two Gaussian Components
 
+rm(list=ls())
+
 #================================================================
 # Model: ========================================================
 #================================================================
@@ -14,36 +16,10 @@
 #	w ~ Beta(1,1)	
 
 #================================================================
-# Likelihood Function ===========================================
-#================================================================
-
-full.lhood = function(d,w,mu,sig.sq){
-	#-------------------------------------------------------------
-	#FUNCTION: 	Generates full likelihood for two-component Gaussian mixture
-	#			with latent indicators d=1, 0 to indicate component for each y.
-	#-------------------------------------------------------------
-	#INPUTS:	d = vector of indicators for components of y, length n.
-	#			w = weight, between 0 and 1.
-	#			mu = vector, (mu1,mu2) of means for the two components.
-	#			sig.sq = vector, (sig.sq1,sig.sq2) of vars for the two components.
-	#-------------------------------------------------------------
-	#OUTPUT:	l = value of full likelihood function.
-	#-------------------------------------------------------------
-	n1 = sum(d==1)
-	n2 = sum(d==0)
-	
-	comp.1 = w * rnorm(n1,mu[1],sqrt(sig.sq[1]))
-	comp.2 = (1-w) * rnorm(n2,mu[2],sqrt(sig.sq[2]))
-	
-	l = prod(c(comp.1,comp.2))
-	return(l)
-}
-
-#================================================================
 # Gibbs Sampler =================================================
 #================================================================
 
-gibbs = function(y,hyperparams=c(0,0,100,100,1,1,1,1),iter=11000,burn=1000,thin=2){
+gibbs = function(y,iter=11000,burn=1000,thin=2){
 	#-------------------------------------------------------------
 	#FUNCTION: 	Gibbs Sampler for two-component gaussian mixture model.
 	#-------------------------------------------------------------
@@ -60,128 +36,153 @@ gibbs = function(y,hyperparams=c(0,0,100,100,1,1,1,1),iter=11000,burn=1000,thin=
 	#			mu2 = vector of posterior mu2 samples.
 	#			lambda = vector of posterior lambda samples.
 	#			w = vector of posterior w samples.
-	#			prob_d = array of posterior prob(d) samples.  
-	#				d[i,j,k] access obs i, d level j, gibbs sample k.
 	#			d = matrix of d assignments.  
 	#				d[i,k] = d value for obs i, gibbs sample k.
+	#			y.pred = vector of draws from predictive distribution.
 	#-------------------------------------------------------------
+	### Data setup.
 	
-	#Extract hyperparameters.
-	m1 = hyperparams[1];	m2 = hyperparams[2];
-	v1 = hyperparams[3];	v2 = hyperparams[4];
-	a = hyperparams[5];		b = hyperparams[6];
-	c = hyperparams[7];		d = hyperparams[8];
+	#Hyperparameters: Prior means and variances for mu1 and mu2.
+	mu0 = c(0,0)
+	v0 = c(100,100)
+	
+	#Hyperparameters for lambda = 1/sig.sq ~ Ga(a,b).
+	a=1
+	b=1
+	
+	#Hyperparameters for w ~ Beta(c,d).
+	c=1
+	d=1
 	
 	n = length(y)	#Total observations.
-	s = 2			#Number of components.
+	M = 2			#Number of components.
 	
 	#Set up structures to hold parameters.
-	mu1 = rep(0,iter)
-	mu2 = rep(0,iter)
-	lambda = rep(0,iter)
-	w = rep(0,iter)
-	d = matrix(0,n,iter)
-	y.pred = rep(0,iter)	#For posterior predictive, ie 
-							#estimating \int g(y|theta)*f(theta|y1...yn) dtheta, with 
-							#theta=(mu1,mu2,w,lambda)
-	
-	#Initialize first iteration values.
-	mu1[1] = 0
-	mu2[1] = 0
-	lambda[1] = 1
-	w[1] = .5					#Initial w = .5
-	d[,1] = rbinom(n,1,w[1])	#Generating random 1's and 0's for d1...dn.
-	
-	#Initialize first y value from predictive distribution.
-	y.pred[1] = full.lhood(d[,1],w[1],mu=c(mu1[1],mu2[1]),sig.sq=c(1/lambda[1],1/lambda[1]))
-	
-	#Iterate through sampler.
-	for (i in 2:iter){
-		
-		#Set up observations in each group based on previous d1...dn.
-		y1 = y[which(d[,i-1]==1)]
-		y2 = y[which(d[,i-1]==0)]
-		ybar1 = mean(y1)
-		ybar2 = mean(y2)
-		n1 = length(y1)
-		n2 = length(y2)
-		
-		#Update mu1.
-		var = 1 / (1/v1 + lambda[i-1]*n1)
-		mean = var * lambda[i-1]*n1*ybar1
-		mu1[i] = rnorm(1,mean,sqrt(var))
-		
-		#Update mu2.
-		var = 1 / (1/v2 + lambda[i-1]*n2)
-		mean = var * lambda[i-1]*n2*ybar2
-		mu2[i] = rnorm(1,mean,sqrt(var))
-		
-		#Update w.
-		w[i] = rbeta(1,n1+1,n2+1)
-		
-		#Update lambda.
-		rt = .5 * ( sum((y1-mu1[i])^2) + sum((y2-mu2[i])^2)  )
-		lambda[i] = rgamma(1,n,rt)
-		
-		#Update d1...dn.
-		num = w[i] * dnorm(y,mu1[i],1/sqrt(lambda[i]))
-		denom = w[i] * dnorm(y,mu1[i],1/sqrt(lambda[i])) + (1-w[i]) * dnorm(y,mu2[i],1/sqrt(lambda[i]))
-		probs.d = num / denom
-		d[,i] = rbinom(n,1,probs.d)
-		
-		#Generate a y value from predictive.
-		y.pred[i] = full.lhood(d[,i],w[i],mu=c(mu1[i],mu2[i]),sig.sq=c(1/lambda[i],1/lambda[i]))
+	mu.j 	= array(NA, dim=c(M,iter))	#Each col is a mu.j.
+	lambda 	= array(NA,dim=c(iter))		#Holds precisions.
+	w.j 	= array(NA,dim=c(iter))		#Holds weights.  
+	d.i 	= array(NA,dim=c(n,iter))	#Holds component memberships.
+	y.pred	= array(NA,dim=c(iter))		#For posterior predictive draws.
 
+	#-------------------------------------------------------------
+	### Initialize first iteration values.
+	mu.j[,1] = 0
+	lambda[1] = .1
+	w.j[1] = .5
+
+	#Generating random 1's and 2's for d1...dn.
+	d.i[,1] = ifelse(rbinom(n,1,w.j[1])==1,1,2) 	
+
+	#Initialize first y value from predictive distribution.
+	component = sample(1:M,1,prob=c(w.j[1],1-w.j[1]))
+	y.pred[1] = rnorm(1,mu.j[component,1], 1 / sqrt(lambda[1]))
+	
+	#-------------------------------------------------------------
+	#Gibbs sampler.
+	for (k in 2:iter){ 
+		
+		### Progress report.
+		if(k %% 100 == 0) {
+			print(sprintf('Iteration %i out of %i...',k,iter))
+		}
+		
+		#---------------------------------------------------------
+		### Update lambda (1/sig.sq).
+		
+		#\sum_{i=1}^{n}(y.i - mu_{d.i})^2
+		mu.di = mu.j[,k-1][d.i[,k-1]]
+		SS = sum((y - mu.di)^2)
+		
+		lambda[[k]] = rgamma(1, n + a, b + SS/2)
+		
+		#---------------------------------------------------------
+		### Update mu.j (normal-normal).
+		nj = as.numeric(table(factor(d.i[,k-1],levels=1:M)))
+		
+		ybar = aggregate(y,list(d.i[,k-1]),mean,drop=F)$x
+
+		#Fill in empty ybars with draws from prior.
+		if(nj[1]==0){ 
+			new.mu = rnorm(1,mu0[1],var[1])
+			ybar = c(new.mu, ybar) 
+		}
+		if(nj[2]==0){ 
+			new.mu = rnorm(1,mu0[2],var[2])
+			ybar = c(ybar, new.mu) 
+		}
+		
+		var = 1 / (1/v0 + nj * lambda[k])
+		mean = var * ((1/v0)*mu0 + nj * lambda[k] * ybar)
+		
+		mu.j[,k] = rnorm(M,mean,sqrt(var))
+		
+		#---------------------------------------------------------
+		### Update w.
+		w.j[k] = rbeta(1,nj[1]+1,nj[2]+1)
+		
+		#---------------------------------------------------------
+		### Update d.i and P(d.i=j).
+		probs.d = w.j[k] * dnorm(y,mu.j[1,k], 1/sqrt(lambda[k])) / 
+			(w.j[k] * dnorm(y,mu.j[1,k], 1/sqrt(lambda[k])) +
+			(1-w.j[k]) * dnorm(y,mu.j[2,k], 1/sqrt(lambda[k])))
+		
+		
+		#num = w.j[k] * dnorm(y,mu.j[1,k],1/sqrt(lambda[k]))
+		#denom = w.j[k] * dnorm(y,mu.j[1,k],1/sqrt(lambda[k])) + (1-w.j[k]) * dnorm(y,mu.j[2,k],1/sqrt(lambda[k]))
+		#probs.d = num / denom
+		d.i[,k] = ifelse(rbinom(n,1,probs.d)==1,1,2) 	
+		
+		#Generate a y value from posterior predictive, using currently updated weight.
+		component = sample(1:M,1,prob=c(w.j[k],1-w.j[k]))
+		y.pred[k] = rnorm(1,mu.j[component,k], 1 / sqrt(lambda[k]))
 	}
 	
 	#Burn beginning observations.
 	if (burn > 0){
-		mu1 = mu1[-burn]
-		mu2 = mu2[-burn]
-		w = w[-burn]
+		mu.j = mu.j[,-burn]
+		w.j = w.j[-burn]
 		lambda = lambda[-burn]
-		d = d[,-burn]
+		d.i = d.i[,-burn]
+		y.pred = y.pred[-burn]
 	}
 	
 	#Thin observations.
 	if (thin > 0){
-		mu1 = mu1[seq(1,length(mu1),by=thin)]
-		mu2 = mu2[seq(1,length(mu2),by=thin)]
-		w = w[seq(1,length(w),by=thin)]
-		lambda = lambda[seq(1,length(lambda),by=thin)]
-		d = d[,seq(1,ncol(d),by=thin)]
+		keep.idx = seq(1,length(lambda),by=thin)
+		mu.j = mu.j[,keep.idx]
+		w.j = w.j[keep.idx]
+		lambda = lambda[keep.idx]
+		d.i = d.i[,keep.idx]
+		y.pred = y.pred[keep.idx]
 	}
 	
 	#Return results.
-	return(list(mu1=mu1,mu2=mu2,w=w,lambda=lambda,d=d,y.pred=y.pred))
+	return(list(mu.j = mu.j, w.j = w.j, lambda = lambda, d.i = d.i, y.pred = y.pred))
 }
 
 #================================================================
-# Generate Data =================================================
+# Generate Data & Run Sampler ===================================
 #================================================================
-
-#Set hyperparameters.
-hyperparameters = c(0,0,100,100,1,1,1,1)
 
 #Generate data of size n=50, with 30 obs from N(-1,4) and 
 #remaining 20 with mean (1,4).
-y = c(rnorm(30,-1,2),rnorm(20,1,2))
-true.w = 30/50
+y = c(rnorm(300,-1,2),rnorm(200,1,2))
 
-#================================================================
-# Run Sampler ===================================================
-#================================================================
-
-K = 11000
-output = gibbs(y,hyperparams=c(0,0,100,100,1,1,1,1),iter=K,burn=1000,thin=2)
+output = gibbs(y,iter=1100,burn=100,thin=2)
 
 #================================================================
 # Investigate Results ===========================================
 #================================================================
 
+### Plot predictive.
+par(mfrow=c(1,2))
+hist(y,main='Posterior Predictive (blue) on Data Hist',breaks=50,freq=F)
+lines(density(output$y.pred),lwd=2,col='blue')
+hist(output$y.pred,breaks=20,freq=F,main='Histogram of Predictive')
+
 ### Check and see if the d values can be used to tell which obs came from each component.
-true.d = c(rep(1,30),rep(0,20))
-est.d = round(rowMeans(output$d),2)
+true.d = c(rep(1,30),rep(2,20))
+est.d = round(rowMeans(output$d.i),2)
 cbind(true.d,est.d)
 
 #Plot d values versus true d values.
@@ -197,11 +198,13 @@ dev.off()
 #Histograms of posterior parameters (reasonableness checking).
 pdf('/Users/jennstarling/UTAustin/2017S_MCMC/Exercises/Exercise-11/Figures/MCMC_Ex11_Prob1_hist.pdf')
 par(mfrow=c(2,2))
-hist(output$mu1,xlab='True mu1: -1')				
-hist(output$mu2,xlab='True mu2: 1')
+hist(output$mu.j[1,],xlab='True mu1: -1')				
+hist(output$mu.j[2,],xlab='True mu2: 1')
 hist(output$w,xlab='True w: .6')
 hist(output$lambda,xlab='True lambda: .01')
 dev.off()
+
+
 
 #	Parameter estimates are generally poor. w was not bad.  Others have some issues:
 #	1. mu1 and mu2 components have switched.  Like in lecture; cannot say which component is really which.
@@ -219,8 +222,14 @@ dev.off()
 y2 = c(rnorm(30,-3,2),rnorm(20,3,2))
 true.w = 30/50
 
-output2 = gibbs(y,hyperparams=c(0,0,100,100,1,1,1,1),iter=11000,burn=1000,thin=2)
+output = gibbs(y,hyperparams=c(0,0,100,100,1,1,1,1),iter=11000,burn=1000,thin=2)
 
+
+### Plot predictive.
+hist(y,main='Posterior Predictive',breaks=20,freq=F)
+lines(density(output$y.pred),lwd=2,col='blue')
+
+# Plot parameters.
 pdf('/Users/jennstarling/UTAustin/2017S_MCMC/Exercises/Exercise-11/Figures/MCMC_Ex11_Prob1_hist2.pdf')
 par(mfrow=c(2,2))
 hist(output2$mu1,xlab='True mu1: -3')				
